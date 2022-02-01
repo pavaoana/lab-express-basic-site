3ec7922d7f8a0bc3b7adf95358eea3246736be9b
Doan Tran <doan7tran@gmail.com>
2022-02-01 16:25:29 +0100

    express structure added

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (request, response, next) => {
+  response.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/about", (request, response, next) => {
+  response.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/works", (request, response, next) => {
+  response.sendFile(__dirname + "/views/works.html");
+});
+
+app.get("/gallery", (request, response, next) => {
+  response.sendFile(__dirname + "/views/gallery.html");
+});
+
+app.listen(4000),
+  () => {
+    console.log("server listening ...");
+  };
